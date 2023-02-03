0b5ef475bee92e7b4833cdbbbb0c50092fa263ee
fvgm <fv.magro@gmail.com>
2023-02-02 22:44:04 -0300

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # c-EventDrivenStateMachine
-An Event-Driven State Machine written in C for Embedded Systems
+A Event-Driven Finite State Machine written in C for Embedded Systems.
+In the programming field, a event-driven state machine is charecterized if the transitions from one state to another is triggered by an event or a message.
+This example is written in standard C language, where a car transmission system can be in one of the following states:
+* Forward
+* Neutral
+* Reverse
+But, we can't go to Reverse gear directly from Forward and vice-versa; we need to pass thru Neutral before. Also, in this simple example, we can turn off the engine (i.e. quit the program) only at the Neutral state.
+
+![State Machine Flowchart](assets/transmission_example.png "State Machine Flowchart").
+The events associated with the states is associated in a bidimensional array, as this example:
+~~~
+void (* State_Event_Matrix[EVENT][STATE]) () =
+{
+    {   // Event Neutral_btn click
+        Not_Valid,      // callback function called when Neutral_btn clicked at NEUTRAL STATE
+        Neutral,        // callback function called when Neutral_btn clicked at FORWARD STATE
+        Neutral,        // callback function called when Neutral_btn clicked at REVERSE STATE
+    },
+    {   // Event Reverse_btn click
+        Reverse,        // State NEUTRAL
+        Not_Valid,      // State FORWARD
+        Not_Valid,      // State REVERSE
+    },
+    {   // Event Forward_btn click
+        Forward,        // State NEUTRAL
+        Not_Valid,      // State FORWARD
+        Not_Valid,      // State REVERSE
+    },
+    {   // Event Quit_btn click
+        QuitProgram,        // State NEUTRAL
+        Not_Valid,          // State FORWARD
+        Not_Valid,          // State REVERSE
+    }
+};
+~~~
+The sequence of the events listed above should be in the same order as declared in the enum:
+~~~
+enum {
+    NEUTRAL,
+    FORWARD,
+    REVERSE
+}; // all the valid states
+
+enum {
+    NEUTRAL_BTN,
+    REVERSE_BTN,
+    FORWARD_BTN,
+    QUIT_BTN
+}; // all the possible events
+~~~
--- main.c
+++ main.c
@@ -1,8 +1,34 @@
+/*
+This is an implementation of a Event Driven Finite State Machine.
+
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+SPDX-License-Identifier: MIT
+Copyright (c) 2023 Fernando Magro and other contributors.
+
+Permission is hereby  granted, free of charge, to any  person obtaining a copy
+of this software and associated  documentation files (the "Software"), to deal
+in the Software  without restriction, including without  limitation the rights
+to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
+copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
+IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
+FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
+AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
+LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /*===========================================================================*/
-// STATE EVENT MACHINE EXAMPLE
+// STATE DRIVEN FINITE STATE MACHINE EXAMPLE
 
 #define EVENT    4
 #define STATE    3
@@ -17,25 +43,25 @@ enum {
     NEUTRAL,
     FORWARD,
     REVERSE
-};
+}; // all the valid states
 
 enum {
     NEUTRAL_BTN,
     REVERSE_BTN,
     FORWARD_BTN,
     QUIT_BTN
-};
+}; // all the possible events
 
-unsigned char currentState = NEUTRAL;
+unsigned char currentState = NEUTRAL; // set the initial state.
 
 
 /*===========================================================================*/
 void (* State_Event_Matrix[EVENT][STATE]) () =
 {
     {   // Event Neutral_btn
-        Not_Valid,      // State NEUTRAL
-        Neutral,        // State FORWARD
-        Neutral,        // State REVERSE
+        Not_Valid,      // callback function called when Neutral_btn clicked at NEUTRAL STATE
+        Neutral,        // callback function called when Neutral_btn clicked at FORWARD STATE
+        Neutral,        // callback function called when Neutral_btn clicked at REVERSE STATE
     },
     {   // Event Reverse_btn
         Reverse,        // State NEUTRAL
